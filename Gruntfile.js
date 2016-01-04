847ca2fce831b8061da569cd2416898149abd640
BorQube <knight666@gmail.com>
2016-01-04 15:31:14 +0100

    Process entries using preprocess library.

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,30 +79,33 @@ module.exports = function(grunt) {
 			smartypants: false
 		});
 
+		var pp = require('preprocess');
+
 		grunt.file.expand(grunt.template.process('<%= SOURCE_PATH %>/pages/*.json')).forEach(function(fullPath) {
 			var filename = fullPath.match(/([^ \/]+?)\.json$/)[1] + '.html';
 			var outputPath = grunt.template.process('<%= OUTPUT_PATH %>/pages/' + filename);
 
 			grunt.log.writeln('Processing "' + fullPath + '" -> "' + outputPath + '".');
 
-			var context = grunt.file.readJSON(fullPath);
+			var entry = grunt.file.readJSON(fullPath);
 
-			var content = "";
-			context['paragraphs'].forEach(function(item) {
-				grunt.log.writeln(item);
+			var context = {
+				'PAGE_TITLE': entry['title'],
+				'PAGE_DESCRIPTION': marked(entry['description']),
+				'PAGE_CONTENT': '',
+			};
+
+			entry['paragraphs'].forEach(function(item) {
 				if (item['header'])
 				{
-					content += marked('### ' + item['header'] + ' ###\n\n');
+					context['PAGE_CONTENT'] += marked('### ' + item['header'] + ' ###\n\n');
 				}
-				content += marked(item['text']);
+				context['PAGE_CONTENT'] += marked(item['text']);
 			});
 
-			var template = grunt.file.read(grunt.template.process('<%= SOURCE_PATH %>/page-template.html'));
-			template = template.replace(/##TITLE##/g, context['title']);
-			template = template.replace(/##DESCRIPTION##/g, marked(context['description']));
-			template = template.replace(/##CONTENT##/g, content);
-
-			grunt.file.write(outputPath, template);
+			var template = grunt.template.process('<%= SOURCE_PATH %>/page-template.html');
+			var processed = pp.preprocess(grunt.file.read(template), context);
+			grunt.file.write(outputPath, processed);
 		});
 	});
 
