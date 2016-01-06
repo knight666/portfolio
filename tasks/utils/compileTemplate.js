adaf9ddee9cab322695f628404687adec5ef2200
Quinten Lansu <knight666+github@gmail.com>
2016-01-06 08:12:15 +0100

    Add footer to pages.

--- tasks/utils/compileTemplate.js
+++ tasks/utils/compileTemplate.js
@@ -3,7 +3,11 @@ var pp = require('preprocess');
 module.exports = function(grunt, template, context, outputName) {
 	grunt.log.writeln('Compiling template "' + template + '".');
 
+	var options = {
+		srcDir: grunt.template.process('<%= TEMPLATES_PATH %>/')
+	}
+
 	var loaded = grunt.template.process('<%= TEMPLATES_PATH %>/' + template + '.html');
-	var processed = pp.preprocess(grunt.file.read(loaded), context);
+	var processed = pp.preprocess(grunt.file.read(loaded), context, options);
 	grunt.file.write(grunt.template.process('<%= OUTPUT_PATH %>/' + (outputName || template) + '.html'), processed);
 };
