36358486bb615569981cfd8f74e2915a2b1ab4b0
Quinten Lansu <knight666+github@gmail.com>
2016-01-22 08:28:40 +0100

    Fix project links on main page.

--- tasks/index.js
+++ tasks/index.js
@@ -9,7 +9,9 @@ module.exports = function(grunt) {
 		var featured = [];
 
 		settings.featured.forEach(function(path) {
-			featured.push(grunt.file.readJSON(projects_path + '/' + path))
+			project = grunt.file.readJSON(projects_path + '/' + path);
+			project.filename = path.match(/([^ \/]+?)\.json$/)[1] + '.html';
+			featured.push(project);
 		});
 
 		context['PROJECT_LIST'] = grunt.project_utils.compileProjectList(grunt, featured, { 'description': true, 'featured': true });
