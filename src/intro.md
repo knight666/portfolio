8ee5a790abd10bde93c66ee4dad37aae9e34edca
Quinten Lansu <knight666@gmail.com>
2024-06-09 13:04:00 +0200

    Import intro from a Markdown file.

---
+++ src/intro.md
@@ -0,0 +1,7 @@
+I'm an experienced front-end developer working in the AAA games industry.
+
+I have shipped multiple AAA video game projects with teams spread all across the globe. I have worked with all kinds of UI frameworks and have even built a few of them myself over the years.
+
+What excites me about front-end development is seeing results quickly and being able to bring creative visions to life. I love working with user experience designers to figure out how to optimize an interaction flow equally as much as I love figuring out an optimal way to grab data from a backend.
+
+And although I mainly work in C++, I built [this portfolio](https://github.com/knight666/portfolio) myself using nothing but HTML and CSS best practices.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,8 @@
 	import AtAGlance from "./widgets/organisms/AtAGlance.svelte";
 	import HomeHeader from "./widgets/organisms/HomeHeader.svelte";
 
+	import intro from '../intro.md?raw';
 	import project from '../projects/far-cry-6.json';
-
-	let intro = `I'm an experienced front-end developer working in the AAA games industry.
-
-I have shipped multiple AAA video game projects with teams spread all across the globe. I have worked with all kinds of UI frameworks and have even built a few of them myself over the years.
-
-What excites me about front-end development is seeing results quickly and being able to bring creative visions to life. I love working with user experience designers to figure out how to optimize an interaction flow equally as much as I love figuring out an optimal way to grab data from a backend.
-
-And although I mainly work in C++, I built [this portfolio](https://github.com/knight666/portfolio) myself using nothing but HTML and CSS best practices.`;
 </script>
 
 <HomeHeader></HomeHeader>
